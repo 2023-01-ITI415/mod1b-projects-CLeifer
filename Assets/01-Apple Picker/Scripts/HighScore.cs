d8063e7685decbd49848a2a201e7f14e6b5f6932
CLeifer <cml370@scarletmail.rutgers.edu>
2023-02-02 19:53:23 -0500

    Working high score counter & updated wood material

--- Assets/01-Apple Picker/Scripts/HighScore.cs
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class HighScore : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static private Text _UI_TEXT;
+    static private int _SCORE = 1000;
+
+    private Text txtCom;
+
+    void Awake()
     {
-        
+        _UI_TEXT = this.GetComponent<Text>();
+
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            SCORE = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", SCORE);
     }
 
-    // Update is called once per frame
-    void Update()
+    static public int SCORE
     {
-        
+        get { return _SCORE; }
+        private set
+        {
+            _SCORE = value;
+
+            if (_UI_TEXT != null)
+            {
+                _UI_TEXT.text = "High Score: " + value.ToString("#,0");
+            }
+        }
+    }
+
+    static public void TRY_SET_HIGH_SCORE(int scoreToTry)
+    {
+        if (scoreToTry <= SCORE) return;
+        SCORE = scoreToTry;
+    }
+    [Tooltip("Check this box to reset the HighScore in PlayerPrefs")]
+    public bool resetHighScoreNow = false;
+
+    void OnDrawGizmos()
+    {
+        if ( resetHighScoreNow)
+        {
+            resetHighScoreNow = false;
+            PlayerPrefs.SetInt("HighScore", 1000);
+            Debug.LogWarning("PlayerPrefs HighScore reset to 1,000.");
+        }
     }
 }
