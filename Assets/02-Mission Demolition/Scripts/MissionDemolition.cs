1bb1c0b67ce5c19c38a0705b156444f8f3dcfa16
CLeifer <cml370@scarletmail.rutgers.edu>
2023-02-09 16:03:02 -0500

    Final Version

--- Assets/02-Mission Demolition/Scripts/MissionDemolition.cs
+++ Assets/02-Mission Demolition/Scripts/MissionDemolition.cs
@@ -54,6 +54,8 @@ public class MissionDemolition : MonoBehaviour
         UpdateGUI();
 
         mode = GameMode.playing;
+
+        FollowCam.SWITCH_VIEW(FollowCam.eView.both);
     }
 
     void UpdateGUI()
@@ -69,6 +71,7 @@ public class MissionDemolition : MonoBehaviour
         if((mode == GameMode.playing) && Goal.goalMet)
         {
             mode = GameMode.levelEnd;
+            FollowCam.SWITCH_VIEW(FollowCam.eView.both);
             Invoke("NextLevel", 2f);
         }
     }
